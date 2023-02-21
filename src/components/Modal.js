c2c8cbefac56382ac4b407be347744569f4fd91f
hamza <hamzaa9176@gmail.com>
2023-02-21 09:38:24 +0100

    slider improvement

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,9 +11,9 @@ const [count, setCount] = useState(counter);
         <div className="modal-content flex flex-col relative justify-center items-center" onClick={(e)=>e.stopPropagation()}>
         
             <div className="modal-footer w-full flex flex-row justify-around p-2 text-white  text-3xl">
-            {count>1?<button className="fixed top-2/4 left-0 rounded-3xl bg-slate-600 p-2" onClick={()=>{setCounter(indexImage=>indexImage-1); }}><FaBackward/></button>:''}
-            <button className='fixed top-2/4 right-0 rounded-3xl bg-slate-600 p-2' onClick={()=>{setCounter(indexImage=>indexImage+1);setCount(prev=>prev+1);if(count===dataa.length-5){updatePage()}}}><FaForward/></button>
-                <button className="fixed right-0 close rounded-3xl bg-slate-600 p-2" onClick={()=>{onClose(false); setCounter(0); setCount(0)}}><FaWindowClose/></button>
+            {count>1?<button className="fixed top-2/4 left-0 rounded-3xl bg-green-600 p-2" onClick={()=>{setCounter(indexImage=>indexImage-1); }}><FaBackward/></button>:''}
+            <button className='fixed top-2/4 right-0 rounded-3xl bg-green-600 p-2' onClick={()=>{setCounter(indexImage=>indexImage+1);setCount(prev=>prev+1);if(count===dataa.length-5){updatePage()}}}><FaForward/></button>
+                <button className="fixed right-0 close rounded-3xl bg-green-600 p-2" onClick={()=>{onClose(false); setCounter(0); setCount(0)}}><FaWindowClose/></button>
             </div>
             <div className="main-content min-w-fit min-h-fit">
                 <img alt="modal" src={data} className=" max-w-screen block"/>
